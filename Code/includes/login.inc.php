657aaa487bc0fa8af24497abe0f098de0f5a33ea
jeremie de vos <00276614@rijnijssel.nl>
2018-01-23 15:17:40 +0100

    Update: 23-1

---
+++ Code/includes/login.inc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Transparent login</title>
+    <link rel="stylesheet" type="text/css "href="style.css">
+  </head>
+  <body>
+    <div class="login-box">
+      <img src="../Images/Profile.png" class="avatar">
+      <h1>login here</h1>
+      <form class="" action="index.html" method="post">
+        <p>username</p>
+        <input type="text" name="username" placeholder="Enter Username">
+        <p>password</p>
+        <input type="password" name="password" placeholder="Enter Password">
+        <input type="submit" name="submit" value="Login">
+        <a href="#">Forget Password?</a>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ Code/index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/login.inc.php';?>
+<?php // NOTE: if(Login wass succesfull)"go to home page"else stay)?>
+<?php
+if(array_key_exists('submit',$_POST)){
+   ();
+} ?>
